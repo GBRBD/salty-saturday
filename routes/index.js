db49b1749f5b0c29fcde0a0143840e720ad68438
GBRBD <bodigaborke@gmail.com>
2017-08-22 19:52:36 +0200

    Stories page, create and update new story

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const storyController = require('../controllers/storyController');
-const {catchErrors} = require('../handlers/errorHandlers');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 // Controllers
 
-// Create or edit a story
+// Shows the create story page
 // TODO: Before showing the story form, check if the user is authenticated
 router.get('/add', storyController.addStory);
+// Creates a new story
 router.post('/add', catchErrors(storyController.createStory));
+// We need to edit the story. See more at '_storyForm.pug' form action.
+router.post('/add/:id', catchErrors(storyController.updateStory));
+
+// Show an actual story
+router.get('/story/:id', catchErrors(storyController.getStoryById));
+
+// Shows every story
+router.get('/stories', catchErrors(storyController.getStories));
+// Render out the edit form so the user can update their story
+router.get('/stories/:id/edit', storyController.editStory);
 
 module.exports = router;
