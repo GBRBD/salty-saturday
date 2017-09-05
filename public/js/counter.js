21867dd7c7100397578cfd815b7d58238ed4f7d8
zedz01 <p.kovzz@gmail.com>
2017-09-05 22:15:17 +0200

    counter stuff

--- public/js/counter.js
+++ public/js/counter.js
@@ -1,1 +1,22 @@
+import { $ } from './modules/bling'
 import moment from 'moment'
+
+// 6 is Saturday
+const today = moment().isoWeekday()
+const day = $('#days-text')
+const left = $('#left-text')
+
+// If we have the HTML element 
+if (day) {
+    const diff = 6 - today
+    if (today == 7) {
+        day.textContent = "6"
+    } else if (today != 6) {
+        day.textContent = diff
+    }
+    if (diff != 1) {
+        left.textContent = "days left"
+    } else {
+        left.textContent = "day left"
+    }
+}
